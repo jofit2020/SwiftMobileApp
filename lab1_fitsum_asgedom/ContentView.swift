2dc92ce25c8a17f608bebad74199f7b78e72910a
Jofit <fitsum.asgedom@georgebrown.ca>
2026-03-15 22:29:11 +0100

    Initial Commit

---
+++ lab1_fitsum_asgedom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lab1_fitsum_asgedom
+//
+//  Created by Serbijos on 15/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lab1_fitsum_asgedom/lab1_fitsum_asgedomApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab1_fitsum_asgedomApp.swift
+//  lab1_fitsum_asgedom
+//
+//  Created by Serbijos on 15/03/26.
+//
+
+import SwiftUI
+
+@main
+struct lab1_fitsum_asgedomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
